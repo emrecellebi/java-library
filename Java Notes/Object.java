1c10d9bc9f51a98ebdca59a8c59165ef00832858
Emre Çelebi <Emre_celebi_199505@hotmail.com>
2021-08-08 18:09:32 +0300

    Add files via upload

---
+++ Java Notes/Object.java
@@ -0,0 +1,13 @@
+/********** Object Sınıfı **********/
+///https://docs.oracle.com/javase/8/docs/api/java/lang/Object.html
+getClass(): Object
+hashCode(): int
+equals(Object): boolean
+toString(): String
+notify(): void
+notifyAll(): void
+wait(): void
+wait(long): void
+wait(long, int): void
+clone(): Object protected
+finalize(): void protected
