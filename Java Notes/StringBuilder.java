058003f146c5d1cd219511b3999729e80008c9d7
Emre Çelebi <Emre_celebi_199505@hotmail.com>
2021-08-08 18:28:55 +0300

    Add files via upload

---
+++ Java Notes/StringBuilder.java
@@ -0,0 +1,43 @@
+/********** StringBuilder Sınıfı **********/
+final extends AbstractStringBuilder implements Serializable, CharSequence
+/// https://docs.oracle.com/javase/8/docs/api/java/lang/StringBuilder.html
+serialVersionUID: long static protected
+StringBuilder()
+StringBuilder(CharSequence)
+StringBuilder(String)
+StringBuilder(int)
+append(CharSequence): StringBuilder
+append(CharSequence, int, int): StringBuilder
+append(Object): StringBuilder
+append(String): StringBuilder
+append(StringBuffer): StringBuilder
+append(boolean): StringBuilder
+append(char): StringBuilder
+append(char[]): StringBuilder
+append(char[], int, int): StringBuilder
+append(double): StringBuilder
+append(float): StringBuilder
+append(int): StringBuilder
+append(long): StringBuilder
+appendCodePoint(int): StringBuilder
+delete(int, int): StringBuilder
+deleteCharAt(int): StringBuilder
+indexOf(String): int
+indexOf(String, int): int
+insert(int, CharSequence): StringBuilder
+insert(int, CharSequence, int, int): StringBuilder
+insert(int, Object): StringBuilder
+insert(int, String): StringBuilder
+insert(int, boolean): StringBuilder
+insert(int, char): StringBuilder
+insert(int, char[]): StringBuilder
+insert(int, char[], int, int): StringBuilder
+insert(int, double): StringBuilder
+insert(int, float): StringBuilder
+insert(int, int): StringBuilder
+insert(int, long): StringBuilder
+lastIndexOf(String): int
+lastIndexOf(String, int): int
+replace(int, int, String): StringBuilder
+reverse(): StringBuilder
+toString(): String
